1b8017c635c0cbf7ee8e48bd84de8a62689efcfc
Adam Jedlička <adajedlicka@gmail.com>
2017-03-29 19:40:08 +0200

    Added option to use exported fields

--- table.go
+++ table.go
@@ -9,15 +9,34 @@ type Table map[*sql.ColumnType][]interface{}
 
 func (t Table) Scan(i interface{}) {
 	if reflect.TypeOf(i).Kind() == reflect.Ptr {
+		var fn reflect.Value
+
 		for k, v := range t {
-			val := reflect.ValueOf(i).MethodByName("Set" + k.Name())
-			if val != reflect.ValueOf(nil) {
+
+			field := reflect.ValueOf(i).Elem().FieldByName(k.Name())
+			if field.IsValid() {
+				fn = field.Addr().MethodByName("Set")
+
 				if reflect.ValueOf(v[0]).IsValid() {
-					val.Call([]reflect.Value{reflect.ValueOf((interface{})(v[0]))})
+					fn.Call([]reflect.Value{reflect.ValueOf((interface{})(v[0]))})
 				} else {
-					val.Call([]reflect.Value{reflect.ValueOf((*interface{})(nil))})
+					fn.Call([]reflect.Value{reflect.ValueOf((*interface{})(nil))})
 				}
+
+				continue
 			}
+
+			fn = reflect.ValueOf(i).MethodByName("Set" + k.Name())
+			if fn != reflect.ValueOf(nil) {
+				if reflect.ValueOf(v[0]).IsValid() {
+					fn.Call([]reflect.Value{reflect.ValueOf((interface{})(v[0]))})
+				} else {
+					fn.Call([]reflect.Value{reflect.ValueOf((*interface{})(nil))})
+				}
+
+				continue
+			}
+
 		}
 	} else if reflect.TypeOf(i).Kind() == reflect.Slice {
 
